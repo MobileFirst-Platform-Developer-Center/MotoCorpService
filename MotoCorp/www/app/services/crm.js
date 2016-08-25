b13f7288eb2b9cfa9c10a1cf739be3f0bedb321e
Yoel Nunez <yoel@nunez.guru>
2016-08-25 10:10:48 -0500

    created crm service skeleton

---
+++ MotoCorp/www/app/services/crm.js
@@ -0,0 +1,18 @@
+app.factory('CRM', function ($rootScope) {
+
+  return {
+    search: function(plate) {
+
+    },
+    getCustomer: function(id) {
+
+    },
+    newCustomer: function(customer) {
+
+    },
+    newVisit: function(customerId, visit) {
+
+    }
+  };
+
+});
