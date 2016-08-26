7e3cc777cf0995b72d58d329ffb694e08a9e98b3
Yoel Nunez <yoel@nunez.guru>
2016-08-26 09:48:25 -0500

    implemented crm service

--- MotoCorp/www/app/services/crm.js
+++ MotoCorp/www/app/services/crm.js
@@ -1,17 +1,41 @@
-app.factory('CRM', function ($rootScope) {
+app.factory('CRM', function () {
 
   return {
-    search: function(plate) {
+    search: function (query) {
+      var req = WLResourceRequest('/adapters/CustomerInfo/customers', WLResourceRequest.POST);
 
+      return req.send(query).then(function (response) {
+        return response.responseJSON;
+      }, function (error) {
+        return WLJQ.Deferred().reject(error.responseJSON).promise();
+      });
     },
-    getCustomer: function(id) {
+    getCustomer: function (id) {
+      var req = WLResourceRequest('/adapters/CustomerInfo/customers/' + id, WLResourceRequest.GET);
 
+      return req.send().then(function (response) {
+        return response.responseJSON;
+      }, function (error) {
+        return WLJQ.Deferred().reject(error.responseJSON).promise();
+      });
     },
-    newCustomer: function(customer) {
+    newCustomer: function (customer) {
+      var req = WLResourceRequest('/adapters/CustomerInfo/customers/', WLResourceRequest.PUT);
 
+      return req.send(customer).then(function (response) {
+        return response.responseJSON;
+      }, function (error) {
+        return WLJQ.Deferred().reject(error.responseJSON).promise();
+      });
     },
-    newVisit: function(customerId, visit) {
+    newVisit: function (customerId, visit) {
+      var req = WLResourceRequest('/adapters/CustomerInfo/customers/' + customerId + '/visits/', WLResourceRequest.PUT);
 
+      return req.send(visit).then(function (response) {
+        return response.responseJSON;
+      }, function (error) {
+        return WLJQ.Deferred().reject(error.responseJSON).promise();
+      });
     }
   };
 
