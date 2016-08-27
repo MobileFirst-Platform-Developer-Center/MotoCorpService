61af42de409f38dd6df7d3f128dd0e71ef9e2343
Robert Puryear <robpuryear@yahoo.com>
2016-08-27 13:40:12 -0400

    Added getCustomer function to test.js

--- MotoCorp/www/app/controllers/test.js
+++ MotoCorp/www/app/controllers/test.js
@@ -1,5 +1,18 @@
 app.controller('TestCtrl', function($scope, $state , CRM){
-  $scope.date = new Date();
+  $scope.customer1 = "";
+
+  $scope.customerOne = function(){
+     CRM.getCustomer(1).then(function (results) {
+      if(results.length === 0) {
+        alert('No match found');
+      } else {
+        $scope.customer1 = results;
+        $scope.$apply();
+      }
+    }).fail(function (error) {
+      alert(error);
+    });
+  };
 
   $scope.$on('$ionicView.enter', function() {
     CRM.test();
