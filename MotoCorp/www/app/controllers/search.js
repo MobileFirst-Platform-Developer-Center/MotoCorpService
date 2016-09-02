cb5166c78759de464712c270512ecccdfc45896d
Robert Puryear <robpuryear@yahoo.com>
2016-09-02 15:19:31 -0400

    Added Scaffold code for Lab

--- MotoCorp/www/app/controllers/search.js
+++ MotoCorp/www/app/controllers/search.js
@@ -1,11 +1,28 @@
 app.controller('SearchCtrl', function ($scope, $state, Auth, CRM) {
-  // Add scope variables here
+  $scope.results = [];
+  $scope.searchType = {
+    name:""
+  };
 
   $scope.addCustomer = function(){
     $state.go('app.new-customer');
   };
 
   $scope.searchPlate = function () {
-    
+  // Get the search payload
+  var search = {};
+  search[$scope.searchType.name] = this.query;
+
+// This sends the search payload to the search function at the CRM service.
+    CRM.search(search).then(function (results) {
+      if(results.length === 0) {
+        alert('No match found');
+      } else {
+        $scope.results = results;
+        $scope.$apply();
+      }
+    }).fail(function (error) {
+      alert(error);
+    }); 
   };
 });
