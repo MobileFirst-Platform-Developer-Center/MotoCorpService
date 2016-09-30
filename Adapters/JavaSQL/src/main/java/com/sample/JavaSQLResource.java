c6082dd1fb13dfaedced8fd11c93afdeb15bd631
THEODORA H. CHENG <tcheng@us.ibm.com>
2016-09-30 01:33:09 -0500

    got get all customers working with schema

--- Adapters/JavaSQL/src/main/java/com/sample/JavaSQLResource.java
+++ Adapters/JavaSQL/src/main/java/com/sample/JavaSQLResource.java
@@ -81,11 +81,12 @@ public class JavaSQLResource {
 	}
 
 	@GET
+	@OAuthSecurity(enabled=false)
 	@Produces("application/json")
 	@Path("/{userId}")
 	public Response getUser(@PathParam("userId") String userId) throws SQLException{
 		Connection con = getSQLConnection();
-		PreparedStatement getUser = con.prepareStatement("SELECT * FROM users WHERE userId = ?");
+		PreparedStatement getUser = con.prepareStatement("SELECT * FROM CUSTOMERS WHERE CustomerID = ?");
 
 		try{
 			JSONObject result = new JSONObject();
@@ -94,10 +95,13 @@ public class JavaSQLResource {
 			ResultSet data = getUser.executeQuery();
 
 			if(data.first()){
-				result.put("userId", data.getString("userId"));
-				result.put("firstName", data.getString("firstName"));
-				result.put("lastName", data.getString("lastName"));
-				result.put("password", data.getString("password"));
+				result.put("CustomerID", data.getInt("CustomerID"));
+				result.put("Name", data.getString("Name"));
+				result.put("LicensePlate", data.getString("LicensePlate"));
+				result.put("Make", data.getString("Make"));
+				result.put("Model", data.getString("Model"));
+				result.put("Vin", data.getString("Vin"));
+
 				return Response.ok(result).build();
 
 			} else{
@@ -119,15 +123,17 @@ public class JavaSQLResource {
 	public Response getAllUsers() throws SQLException{
 		JSONArray results = new JSONArray();
 		Connection con = getSQLConnection();
-		PreparedStatement getAllUsers = con.prepareStatement("SELECT * FROM names");
+		PreparedStatement getAllUsers = con.prepareStatement("SELECT * FROM CUSTOMERS");
 		ResultSet data = getAllUsers.executeQuery();
 
 		while(data.next()){
 			JSONObject item = new JSONObject();
-			item.put("name", data.getString("name"));
-			// item.put("firstName", data.getString("firstName"));
-			// item.put("lastName", data.getString("lastName"));
-			// item.put("password", data.getString("password"));
+			item.put("CustomerID", data.getInt("CustomerID"));
+			item.put("Name", data.getString("Name"));
+			item.put("LicensePlate", data.getString("LicensePlate"));
+			item.put("Make", data.getString("Make"));
+			item.put("Model", data.getString("Model"));
+			item.put("VIN", data.getString("VIN"));
 
 			results.add(item);
 		}
@@ -146,7 +152,7 @@ public class JavaSQLResource {
 								@FormParam("password") String password)
 										throws SQLException{
 		Connection con = getSQLConnection();
-		PreparedStatement getUser = con.prepareStatement("SELECT * FROM users WHERE userId = ?");
+		PreparedStatement getUser = con.prepareStatement("SELECT * FROM CUSTOMERS WHERE CustomerID = ?");
 
 		try{
 			getUser.setString(1, userId);
