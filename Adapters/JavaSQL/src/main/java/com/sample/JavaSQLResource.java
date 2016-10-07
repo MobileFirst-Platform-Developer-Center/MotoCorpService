22f309d32cc16345f3000e360b7a35138129ce64
THEODORA H. CHENG <tcheng@us.ibm.com>
2016-10-07 00:14:00 -0500

    added sql query to join tables still need to parse through multi row results

--- Adapters/JavaSQL/src/main/java/com/sample/JavaSQLResource.java
+++ Adapters/JavaSQL/src/main/java/com/sample/JavaSQLResource.java
@@ -47,39 +47,7 @@ public class JavaSQLResource {
 		return app.dataSource.getConnection();
 	}
 
-
-	@POST
-	public Response createUser(@FormParam("userId") String userId,
-								@FormParam("firstName") String firstName,
-								@FormParam("lastName") String lastName,
-								@FormParam("password") String password)
-										throws SQLException{
-
-		Connection con = getSQLConnection();
-		PreparedStatement insertUser = con.prepareStatement("INSERT INTO users (userId, firstName, lastName, password) VALUES (?,?,?,?)");
-
-		try{
-			insertUser.setString(1, userId);
-			insertUser.setString(2, firstName);
-			insertUser.setString(3, lastName);
-			insertUser.setString(4, password);
-			insertUser.executeUpdate();
-			//Return a 200 OK
-			return Response.ok().build();
-		}
-		catch (Exception E) {
-			//Trying to create a user that already exists
-			return Response.status(Status.CONFLICT).entity(E.getMessage()).build();
-		}
-		finally{
-			//Close resources in all cases
-			insertUser.close();
-			con.close();
-		}
-
-
-	}
-
+	// This gets a customer by a customer ID
 	@GET
 	@OAuthSecurity(enabled=false)
 	@Produces("application/json")
@@ -120,6 +88,7 @@ public class JavaSQLResource {
 		return Response.ok().build();
 	}
 
+	// This gets all customers
 	@GET
 	@OAuthSecurity(enabled=false)
 	@Produces("application/json")
@@ -148,72 +117,52 @@ public class JavaSQLResource {
 		return Response.ok(results).build();
 	}
 
-	@PUT
-	@Path("/{userId}")
-	public Response updateUser(@PathParam("userId") String userId,
-								@FormParam("firstName") String firstName,
-								@FormParam("lastName") String lastName,
-								@FormParam("password") String password)
-										throws SQLException{
+	// [In-Progress][GET] Get customer by id with all related visits
+	@GET
+	@OAuthSecurity(enabled=false)
+	@Produces("application/json")
+	@Path("/{userId}/CustomInfoVisits")
+	public Response getUserWithVisits(@PathParam("userId") String userId) throws SQLException{
 		Connection con = getSQLConnection();
-		PreparedStatement getUser = con.prepareStatement("SELECT * FROM CUSTOMERS WHERE 'CustomerID' = ?");
+		PreparedStatement getUser = con.prepareStatement("SELECT * FROM CUSTOMERS WHERE \"CustomerID\" = ?");
+		PreparedStatement getUserInfo= con.prepareStatement("SELECT *\n" +
+				"FROM   CUSTOMERS LEFT OUTER JOIN VISITS\n" +
+				"ON     CUSTOMERS.\"CustomerID\" = VISITS.\"CustomerID\"\n" +
+				"WHERE CUSTOMERS.\"CustomerID\" = ?");
 
 		try{
-			getUser.setString(1, userId);
+			JSONObject result = new JSONObject();
+
+			getUser.setInt(1, Integer.parseInt(userId));
 			ResultSet data = getUser.executeQuery();
 
-			if(data.first()){
-				PreparedStatement updateUser = con.prepareStatement("UPDATE users SET firstName = ?, lastName = ?, password = ? WHERE userId = ?");
+			// need to figure out how to parse through sql results
+			// http://stackoverflow.com/questions/7643576/java-looping-through-result-set
+			if(data.next()){
+				result.put("CustomerID", data.getInt("CustomerID"));
+				result.put("Name", data.getString("Name"));
+				result.put("LicensePlate", data.getString("LicensePlate"));
+				result.put("Make", data.getString("Make"));
+				result.put("Model", data.getString("Model"));
+				result.put("Vin", data.getString("Vin"));
 
-				updateUser.setString(1, firstName);
-				updateUser.setString(2, lastName);
-				updateUser.setString(3, password);
-				updateUser.setString(4, userId);
-
-				updateUser.executeUpdate();
-				updateUser.close();
-				return Response.ok().build();
-
+				return Response.ok(result).build();
 
 			} else{
-				return Response.status(Status.NOT_FOUND).entity("User not found...").build();
+				return Response.status(Status.NOT_FOUND).entity("User information not found...").build();
 			}
+
+		}
+		catch(Exception E) {
+			E.printStackTrace();
 		}
 		finally{
 			//Close resources in all cases
 			getUser.close();
 			con.close();
 		}
-
+		return Response.ok().build();
 	}
 
-	@DELETE
-	@Path("/{userId}")
-	public Response deleteUser(@PathParam("userId") String userId) throws SQLException{
-		Connection con = getSQLConnection();
-		PreparedStatement getUser = con.prepareStatement("SELECT * FROM users WHERE userId = ?");
-
-		try{
-			getUser.setString(1, userId);
-			ResultSet data = getUser.executeQuery();
-
-			if(data.first()){
-				PreparedStatement deleteUser = con.prepareStatement("DELETE FROM users WHERE userId = ?");
-				deleteUser.setString(1, userId);
-				deleteUser.executeUpdate();
-				deleteUser.close();
-				return Response.ok().build();
-
-			} else{
-				return Response.status(Status.NOT_FOUND).entity("User not found...").build();
-			}
-		}
-		finally{
-			//Close resources in all cases
-			getUser.close();
-			con.close();
-		}
-
-	}
-
+	// [TODO][POST] Search customer using parameter passed (talk to Rob about this)
 }
